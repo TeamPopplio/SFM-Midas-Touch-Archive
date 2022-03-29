64e1d0b8f07496a97ceada2ea8eb4bb3b5e59d3b
TeamPopplio <teampopplio@protonmail.com>
2022-03-28 22:51:04 -0700

    Hook into main window and animation set editor.

--- midas/features/sfm_test.cpp
+++ midas/features/sfm_test.cpp
@@ -1,16 +1,29 @@
 #include "stdafx.h"
 #include "..\feature.hpp"
+#include <QtCore>
+#include <QtGui>
+#include <QMetaObject>
 
-typedef void(__fastcall* _GetModelViewerOpened)(void* thisptr, int edx);
+typedef void* (__fastcall* _MainWindowInteracted)(void* thisptr, int edx);
 
-// Feature description
+// Test for SFM functionality.
 class SFMTest : public FeatureWrapper<SFMTest>
 {
 public:
-	static void __fastcall HOOKED_GetModelViewerOpened(void* thisptr, int edx);
+	static void* __fastcall HOOKED_MainWindowChanged(void* thisptr, int edx);
+
+	// Error flags:
+	// 1 - Animation set editor not found.
+	int AcknowledgedErrorFlags;
+
+	int AnimationSetEditorMaxAttempts = 1000;
+	int AnimationSetEditorAttempts = AnimationSetEditorMaxAttempts;
+
+	QWidget* MainWindow;
+	QWidget* AnimationSetEditor;
 
 protected:
-	_GetModelViewerOpened ORIG_GetModelViewerOpened = nullptr;
+	_MainWindowInteracted ORIG_MainWindowChanged = nullptr;
 
 	virtual bool ShouldLoadFeature() override
 	{
@@ -19,12 +32,12 @@ protected:
 
 	virtual void InitHooks() override
 	{
-		HOOK_FUNCTION(ifm, GetModelViewerOpened);
+		HOOK_FUNCTION(ifm, MainWindowChanged);
 	}
 
 	virtual void LoadFeature() override
 	{
-
+		
 	}
 
 	virtual void UnloadFeature() override
@@ -35,9 +48,59 @@ protected:
 
 static SFMTest sfm_test;
 
-void __fastcall SFMTest::HOOKED_GetModelViewerOpened(void* thisptr, int edx)
+void* __fastcall SFMTest::HOOKED_MainWindowChanged(void* thisptr, int edx)
 {
-	sfm_test.ORIG_GetModelViewerOpened(thisptr, edx);
-	Msg("Hello from Midas Touch!");
-	return;
+	if (sfm_test.MainWindow == nullptr)
+	{
+		foreach(QWidget * widget, qApp->topLevelWidgets())
+		{
+			if (widget->inherits("QMainWindow"))
+			{
+				sfm_test.MainWindow = widget;
+				Msg("Midas Touch: Found main window.\n");
+				break;
+			}
+		}
+	}
+	if (sfm_test.MainWindow != nullptr)
+	{
+		// Replace title of SFM.
+		QString windowTitle = sfm_test.MainWindow->windowTitle();
+		QString replaceString = QString::fromAscii("Source Filmmaker [Beta]");
+		QString afterString = QString::fromAscii("SFM: Midas Touch");
+		sfm_test.MainWindow->setWindowTitle(windowTitle.replace(replaceString, afterString));
+		// Find animation set editor.
+		if (sfm_test.AnimationSetEditor == nullptr)
+		{
+			QWidget* widget = sfm_test.MainWindow->findChild<QWidget*>(QString::fromAscii("QAnimationSetEditor"));
+			if (widget != nullptr)
+			{
+				Msg("Midas Touch: Found animation set editor in %d attempts.\n", sfm_test.AnimationSetEditorAttempts);
+				sfm_test.AnimationSetEditor = widget;
+			}
+			else if (sfm_test.AnimationSetEditorAttempts > 0)
+			{
+				sfm_test.AnimationSetEditorAttempts--;
+			}
+			else if ((sfm_test.AcknowledgedErrorFlags & 1) == 0)
+			{
+				// Show error modal.
+				Msg("Midas Touch: Failed to find animation set editor within %d attempts.\n", sfm_test.AnimationSetEditorMaxAttempts);
+				sfm_test.AcknowledgedErrorFlags = 1 >> sfm_test.AcknowledgedErrorFlags;
+				QMessageBox messageBox;
+				messageBox.critical(0, "Midas Touch: Error", "Could not find the animation set editor widget!\nMidas Touch may not function correctly.\nPlease open the animation set editor widget.");
+			}
+		}
+	}
+	return sfm_test.ORIG_MainWindowChanged(thisptr, edx);
+}
+
+QWidget* GetMainWindow()
+{
+	return sfm_test.MainWindow;
+}
+
+QWidget* GetAnimationSetEditor()
+{
+	return sfm_test.AnimationSetEditor;
 }
